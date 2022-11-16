20b633b12b2c6b5eda625b1cf0d81f47ec93183e
Ryan <ryanestanislao@gmail.com>
2022-11-16 10:12:36 -0800

    yes

--- app/src/main/java/com/example/loginandregistration/LoginActivity.kt
+++ app/src/main/java/com/example/loginandregistration/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.loginandregistration
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.loginandregistration.databinding.ActivityLoginBinding
@@ -13,5 +15,10 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.textView.setOnClickListener {
+            val registrationIntent = Intent(this, RegistrationActivity::class.java)
+            startActivity(registrationIntent)
+        }
     }
 }
